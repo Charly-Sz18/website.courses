665c888068b9de5e77d799f9324d88e8c996c280
Gaby <a20216884@alumnos.uady.mx>
2023-11-22 18:31:33 -0600

    Intente hacer lo de categorias

--- src/views/CategoryView.vue
+++ src/views/CategoryView.vue
@@ -1,12 +1,11 @@
 <template>
     <HeaderUser> </HeaderUser>
     <div> {{ category }}</div>
-    <CourseCardVue> </CourseCardVue>
-    <v-row>
+<!--     <v-row>
       <v-col v-for="curso in filteredCourses" :key="curso.id" cols="12" md="4">
         <CourseCardVue :curso="curso"></CourseCardVue>
       </v-col>
-    </v-row>
+    </v-row> -->
     
 </template>
 
@@ -27,9 +26,8 @@ import HeaderUser from '@/components/HeaderUser.vue';
       return this.$store.state.courses.filter(course => course.category === this.category);
     }, */
     filteredCourses() {
-      // Aquí debes acceder a los datos de cursos específicos de tu vista "MyCurses"
-      // Supongamos que tienes una propiedad en tu vista llamada "cursos"
-      const cursos = this.$store.state.myCourses.courses; // Ajusta según la estructura de tu store o datos
+      
+      const cursos = this.$store.state.MyCourses.courses; 
 
       return this.selectedCategory === 'Todos los cursos'
         ? cursos
